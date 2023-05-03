58c9ce7eb52a80828fbff41e775b985c28376d2a
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2023-05-03 18:46:16 +0900

    add

--- DqnMoonLanding.py
+++ DqnMoonLanding.py
@@ -13,7 +13,7 @@ import cv2
 learning_rate = 0.0005
 gamma = 0.98
 buffer_limit = 100000
-batch_size = 32
+batch_size = 64
 
 
 class ReplayBuffer():
@@ -38,57 +38,95 @@ class ReplayBuffer():
             s_prime_lst.append(s_prime)
             done_mask_lst.append([done_mask])
 
-        return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst), \
-            torch.tensor(r_lst), torch.tensor(s_prime_lst, dtype=torch.float), \
-            torch.tensor(done_mask_lst)
+        return torch.tensor(s_lst, dtype=torch.float), torch.tensor(a_lst, dtype=torch.float), \
+            torch.tensor(r_lst, dtype=torch.float), torch.tensor(s_prime_lst, dtype=torch.float), \
+            torch.tensor(done_mask_lst, dtype=torch.float)
+
+    def size(self):
+        return len(self.buffer)
 
 
 class Qnet(nn.Module):
     def __init__(self) -> None:
         super(Qnet, self).__init__()
-        self.fc1 = nn.Linear(8, 128)
-        self.fc2 = nn.Linear(128, 240)
-        self.fc3 = nn.Linear(240, 4)
+        self.fc1 = nn.Linear(8, 64)
+        self.fc2 = nn.Linear(64, 128)
+        self.fc3 = nn.Linear(128, 4)
 
     def forward(self, x):
-        # 활성함수 relu 사용 0~무한
         x = F.relu(self.fc1(x))
-        # 활성함수 relu 사용 0~무한
         x = F.relu(self.fc2(x))
-        # 마지막에는 활성함수 X
-        # 원래 마지막에는 활성함수 안넣음
         x = self.fc3(x)
         return x
 
     def sample_action(self, obs, epsilon):
-        # 입실론 그리디 구현을위한 함수
-        #
-        print(obs)
         out = self.forward(obs)
-        # 코인이라는 변수에 랜덤값(0~1) 넣고
         coin = random.random()
         if coin < epsilon:
-            print("나는 랜덤하지롱")
             return random.randint(0, 3)  # 출력 0 또는3 랜덤값
         else:
-            # print(out)
-            # print(out.argmax().item())
-            print("나는 운명적이다")
             return out.argmax().item()
 
 
+def train(q, q_target, memory, optimizer):
+    for i in range(10):
+        s, a, r, s_prime, done_mask = memory.sample(batch_size)
+        q_out = q(s)
+        q_a = q_out.gather(1, a.long())
+        max_q_prime = q_target.forward(s_prime).max(1)[0].unsqueeze(1)
+        target = r + gamma * max_q_prime * done_mask
+        loss = F.smooth_l1_loss(q_a, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
 def main():
-    env = gym.make('LunarLander-v2',  render_mode='human')
+    env = gym.make('LunarLander-v2',  render_mode='rgb_array')
 
-    # q 네트워크
     q = Qnet()
-    s, _ = env.reset()
+    q_target = Qnet()
+    q_target.load_state_dict(q.state_dict())
+    memory = ReplayBuffer()
 
-    # 학습될수록 epsilon값 줄어들음
-    epsilon = 0.5
+    print_interval = 20
+    score = 0.0
+
+    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+
+    for epiIndex in range(10000):
+        epsilon = max(0.01, 0.5 - 0.01*(epiIndex/200))
+        s, _ = env.reset()
+        done = False
+        time = 0
+
+        for t in range(1000):
+            time += 1
+            a = q.sample_action(torch.from_numpy(s).float(), epsilon)
+            s_prime, r, done, truncated, info = env.step(a)
+            done_mask = 0.0 if done else 1.0
+            memory.put((s, a, r/100.0, s_prime, done_mask))
+            s = s_prime
+
+            score += r
+            if epiIndex % 500 == 0 and epiIndex != 0:
+                img = cv2.cvtColor(env.render(), cv2.COLOR_RGB2BGR)
+                cv2.imshow("test", img)
+                cv2.waitKey(30)
+            if done:
+                break
+        if memory.size() > 5000:
+            train(q, q_target, memory, optimizer)
+        if epiIndex % print_interval == 0 and epiIndex != 0:
+            q_target.load_state_dict(q.state_dict())
+            print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                epiIndex, score/print_interval, memory.size(), epsilon*100))
+            score = 0.0
+
+    env.close()
 
     print(s)
-    a = q.sample_action(torch.from_numpy(s).float(), epsilon)
+
     print('a')
     print(a)
 
